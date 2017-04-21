298151e888712fb3be41ad27c25891da16b95c04
Sven <sven@gmail.com>
2017-04-21 17:07:42 +0200

    restructere folder layout

--- startup.sh
+++ startup.sh
@@ -8,11 +8,17 @@
 dir="$(dirname "$0")"
 source $dir/data/links.sh
 
-google-chrome --new-window $LINKS
-google-chrome --new-window $LINKS2
+google-chrome \
+	--new-window \
+	--window-position=1000,1000 \
+	--window-size=2000,1000 \
+	--chrome-frame \
+	$LINKS
+
+#google-chrome --new-window $LINKS2
 
 #charm
 
-devilspie2
+#devilspie2
 
 
